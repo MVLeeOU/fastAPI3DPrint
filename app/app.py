cd660978739160552f0634b2cadc0903b65da19c
Andersen Ball <andersen.ball@gmail.com>
2023-12-03 12:30:12 -0500

    add http errors to web apis

--- app/app.py
+++ app/app.py
@@ -6,18 +6,15 @@ import time
 import json
 import shutil
 import os
+
 timestr = time.strftime("%Y%m%d-%H%M%S")
-from fastapi import FastAPI, File, UploadFile,Body
-
-from fastapi.exceptions import HTTPException
-
+from fastapi import FastAPI, File, UploadFile, Body, HTTPException
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_DIR = "uploads"
 
 # Ensure the UPLOAD_DIR exists
 os.makedirs(UPLOAD_DIR, exist_ok=True)
-
 
 app = FastAPI()
 
@@ -26,38 +23,49 @@ orders = []
 
 @app.get("/")
 def read_root():
-    return {"Hello":"Fast"}
+    return {"Hello": "Fast"}
 
-@app.post("/multiple_inputs")
-async def create_item(
-    param1: int = Body(...),
-    param2: str = Body(...),
-    param3: float = Body(...),
-):
-    return {"param1": param1, "param2": param2, "param3": param3}
 
 @app.post("/checkout/newOrder")
-def new_order(street: int = Body(...),
+def new_order(
+    street: int = Body(...),
     town: str = Body(...),
     postal: int = Body(...),
-    modelName: str = Body(...),userName: str = Body(...),):
+    modelName: str = Body(...),
+    userName: str = Body(...),
+):
+    try:
+        neworder = Order(Address(street, town, postal), modelName, userName)
+        orders.append(neworder)
+        return {"Hello": str(orders[-1])}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
-    neworder = Order(Address(street,town,postal),modelName,userName)
-    orders.append(neworder)
-    return {"Hello":str(orders[-1])}
 
 @app.get("/checkout/allorders")
-def read_root():
-    return {str(orders)}
+def read_all_orders():
+    return {"orders": [str(order) for order in orders]}
+
 
 @app.get("/checkout/orders/{orderID}")
-def read_root(orderID:int):
-    return {str(orders[orderID])}
+def read_order(orderID: int):
+    try:
+        return {"order": str(orders[orderID])}
+    except IndexError:
+        raise HTTPException(status_code=404, detail="Order not found")
+
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
-    return Upload_File(file)
+    try:
+        return Upload_File(file)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 @app.get("/calcfileCost/{filename}")
 async def calcfileCost(filename: str):
-    return Calc_Cost(filename)
+    try:
+        return Calc_Cost(filename)
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="File not found")
